1229ea34b299dc35ec691cb8989b99a87bb1f7a0
anugoyal998 <anugoyal998@gmail.com>
2022-07-22 23:05:09 +0530

    trie

---
+++ Data Structures & Algorithms, Level-up for Coding Interviews/trie/implement_trie.cpp
@@ -0,0 +1,93 @@
+// https://www.codingninjas.com/codestudio/problems/implement-trie_631356?utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_tries_videos
+
+#include "bits/stdc++.h"
+using namespace std;
+
+/*
+    Your Trie object will be instantiated and called as such:
+    Trie* obj = new Trie();
+    obj->insert(word);
+    bool check2 = obj->search(word);
+    bool check3 = obj->startsWith(prefix);
+ */
+
+struct Node
+{
+    Node* links[26];
+    bool flag = false;
+
+    bool containsKey(char ch){
+        return links[ch-'a'] != NULL;
+    }
+
+    void put(char ch,Node* node){
+        links[ch-'a'] = node;
+    }
+
+    Node* get(char ch){
+        return links[ch-'a'];
+    }
+
+    void setEnd(){
+        flag = true;
+    }
+
+    bool isEnd(){
+        return flag;
+    }
+};
+
+
+class Trie {
+private: Node* root;
+public:
+
+    /** Initialize your data structure here. */
+    Trie() {
+        root = new Node();
+    }
+
+    /** Inserts a word into the trie. */
+    /* TC: O(N) */
+    void insert(string word) {
+        Node* node = root;
+        for(int i=0;i<word.length();i++){
+            if(!node->containsKey(word[i])){
+                node->put(word[i], new Node());
+            }
+            node->get(word[i]);
+        }
+        node->setEnd();
+    }
+
+    /** Returns if the word is in the trie. */
+    /* TC: O(N) */
+    bool search(string word) {
+        Node* node = root;
+        for(int i = 0; i < word.length(); i++){
+            if(!node->containsKey(word[i])){
+                return false;
+            }
+            node = node->get(word[i]);
+        }
+        return node->isEnd();
+    }
+
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    /* TC: O(N) */
+    bool startsWith(string prefix) {
+        Node* node = root;
+        for(int i = 0; i < prefix.length();i++){
+            if(!node->containsKey(prefix[i])){
+                return false;
+            }
+            node = node->get(prefix[i]);
+        }
+        return true;
+    }
+};
+
+int main(){
+    
+    return 0;
+}
